d8846a2db5f843e65af6b032b314b2e4386d22e0
Mohit Prakash <m2450p@gmail.com>
2023-06-08 21:51:54 +0530

    Hybernate validation if failed then handle exception like this

--- src/main/java/com/mps/blogapp/entity/User.java
+++ src/main/java/com/mps/blogapp/entity/User.java
@@ -5,7 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="user_tab")
@@ -16,6 +18,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    @NotEmpty
+    @Size(min = 3,message = "Size should be atleast 3")
     private String username;
     @Pattern(regexp = "^[a-z0-9]{3,10}[/@][a-z]{3,10}[.][a-z]{3,7}$",message = "Email should be in proper format!!")
     private String userEmail;
--- src/main/java/com/mps/blogapp/exception/RestExceptionHandler.java
+++ src/main/java/com/mps/blogapp/exception/RestExceptionHandler.java
@@ -3,33 +3,28 @@ package com.mps.blogapp.exception;
 import com.mps.blogapp.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException manve){
-        String msg=manve.toString();
-        System.out.println(manve.getMessage());
-        List<String> errors = Arrays.asList(msg).stream().map(str -> {
-            int startIndex = 0, endIndex = str.length() - 1;
-            for (int i = str.length() - 1; i >= 0; i--) {
-                if (str.charAt(i) == ']') {
-                    endIndex--;
-                } else if (str.charAt(i) == '[') {
-                    startIndex = i + 1;
-                    break;
-                }
-            }
-            return str.substring(startIndex, endIndex);
-        }).collect(Collectors.toList());
-        return new ResponseEntity<>(new ApiResponse(errors.get(0), false), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException manve){
+        Map<String,String> resp = new HashMap<>();
+        manve.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String errorMsg = error.getDefaultMessage();
+            resp.put(fieldName,errorMsg);
+        });
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
     }
 }
